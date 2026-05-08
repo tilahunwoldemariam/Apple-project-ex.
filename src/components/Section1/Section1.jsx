34f29599f3a691965ca8a88e52bc168167e957c4
tilahunwoldemariam <tilahunkw12@gmail.com>
2026-05-08 04:43:54 -0500

    Remove alert title from Section1 component
    Removed alert title text from Section1 component.

--- src/components/Section1/Section1.jsx
+++ src/components/Section1/Section1.jsx
@@ -5,9 +5,7 @@ function Section1() {
     <div>
       <section className="alert-section top-50">
 		<div className="container">
-			<div className="alert-title">
-				We’re open for you.
-			</div>
+			
 		</div>
 	</section>
     </div>
