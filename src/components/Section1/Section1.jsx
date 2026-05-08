0532ce85e00371ff81c7fea36f308666ad8d6fb4
tilahunwoldemariam <tilahunkw12@gmail.com>
2026-05-08 04:24:28 -0500

    Remove alert text from Section1 component
    Removed alert text regarding retail store closure and online support.

--- src/components/Section1/Section1.jsx
+++ src/components/Section1/Section1.jsx
@@ -8,10 +8,6 @@ function Section1() {
 			<div className="alert-title">
 				We’re open for you.
 			</div>
-			<div className="alert-text">
-				Our retail stores are closed, but you can buy our products here online and get fast, free delivery. If you need help finding the right product or have a question on your order, chat online with a Specialist or call 1-800-MY-APPLE.<br/>
-				For service and support, visit <a href="https://support.apple.com/">support.apple.com</a>.
-			</div>
 		</div>
 	</section>
     </div>
